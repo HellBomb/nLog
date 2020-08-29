d4243644ea2ecb3cae8a394266050ca847e9fb9b
HellBomb <33955773+HellBomb@users.noreply.github.com>
2020-08-28 21:53:52 -0400

    Update Write-nLog.ps1

--- Write-nLog.ps1
+++ Write-nLog.ps1
@@ -7,18 +7,16 @@ Function Write-nLog {
             Easy and highly functional logging function that can be dropped into any script to add logging capability without hindering script performance.
 
         .PARAMETER type
-            Set the event level of the log event. 
-
+            Set the event level of the log event.
             [Options]
                 Info, Warning, Error, Debug
         
         .PARAMETER message
             Set the message text for the event.
 
-
         .PARAMETER ErrorCode
-            Set the Error code for Error & fatal level events. The error code will be displayed in front of 
-            the message text for the event.
+            Set the Error code for Error & fatal level events. The error code will be displayed in front of the message text for the event.
+            
 
         .PARAMETER WriteHost
             Force writing to host reguardless of SetWriteLog setting for this specific instance.
@@ -30,19 +28,14 @@ Function Write-nLog {
             Set the log level for the nLog function for all future calls. When setting a log level all logs at 
             the defined level will be logged. If you set the log level to warning (default) warning messages 
             and all events above that such as error and fatal will also be logged. 
-
             (1) Debug: Used to document events & actions within the script at a very detailed level. This level 
             is normally used during script debugging or development and is rarely set once a script is put into
             production
-
             (2) Information: Used to document normal application behavior and milestones that may be useful to 
             keep track of such. (Ex. File(s) have been created/removed, script completed successfully, etc)
-
             (3) Warning: Used to document events that should be reviewed or might indicate there is possibly
             unwanted behavior occuring.
-
             (4) Error: Used to document non-fatal errors indicating something within the script has failed.
-
             (5) Fatal: Used to document errors significant enough that the script cannot continue. When fatal
             errors are called with this function the script will terminate. 
         
@@ -56,16 +49,17 @@ Function Write-nLog {
 
         .PARAMETER SetWriteHost
             Configure if the script should write events to the screen. (Default: $False)
-
             [Options]
                 $True,$False
         
         .PARAMETER SetWriteLog
             Configure if the script should write events to the screen. (Default: $True)
-
             [Options]
                 $True,$False
         
+        .PARAMETER Close
+            Removes all script-level variables set while nLog creates while running.
+
         .INPUTS
             None
 
@@ -75,13 +69,12 @@ Function Write-nLog {
         .NOTES
         VERSION     DATE			NAME						DESCRIPTION
 	    ___________________________________________________________________________________________________________
-	    1.0         25 May 2020		HellBomb					Initial version
+	    1.0			25 May 2020		Warila, Nicholas R.			Initial version
+        2.0			28 Aug 2020		Warila, Nicholas R.			Complete rewrite of major portions of the script, significant improvement in script performance (about 48%), and updated log format.
 
         Credits:
             (1) Script Template: https://gist.github.com/9to5IT/9620683
     #>
-
-    [CmdletBinding()]
     Param (
         [Parameter(Mandatory=$True,Position=0)]
         [ValidateSet('Debug','Info','Warning','Error','Fatal')]
@@ -89,13 +82,9 @@ Function Write-nLog {
         [Parameter(Mandatory=$True,ValueFromPipeline=$False,Position=1)]
         [String[]]$Message,
         [Parameter(Mandatory=$False,ValueFromPipeline=$False,Position=2)][ValidateRange(0,9999)]
-        [Int]$ErrorCode,
-        
-        #Trigger per-call write-host/write-log 
+        [Int]$ErrorCode = 0,
         [Switch]$WriteHost,
         [Switch]$WriteLog,
-
-        #Variables used to trigger setting global variables.
         [Switch]$Initialize,
         [Parameter(Mandatory=$False,ValueFromPipeline=$False)][ValidateRange(1,5)]
         [Int]$SetLogLevel,
@@ -104,101 +93,169 @@ Function Write-nLog {
         [Parameter(Mandatory=$False,ValueFromPipeline=$False)]
         [Bool]$SetWriteHost,
         [Parameter(Mandatory=$False,ValueFromPipeline=$False)]
-        [Bool]$SetWriteLog
+        [Bool]$SetWriteLog,
+        [Switch]$Close
     )
 
-    Begin {
-        #Get Timestamp from when nLog was called.
-        [DateTime]$tTimeStamp = [DateTime]::Now
-        [Bool]$tLog = $False
+    #Best practices to ensure function works exactly as expected, and prevents adding "-ErrorAction Stop" to so many critical items.
+    $Local:ErrorActionPreference = 'Stop'
+    Set-StrictMode -Version Latest
 
-        #Ensure all the default script-level variables are set.
-        IF ((-Not (Test-Path variable:Script:nLogInitialize)) -OR $Initialize) {
-            New-Variable -Name nLogLevel -Value 3 -Scope Script -Force
-            New-Variable -Name nLogInitialize -Value $True -Force -ErrorAction SilentlyContinue -Scope Script
-            IF (Test-Path variable:global:psISE) {
-                New-Variable -Name nLogWriteHost -Value $True  -Scope Script -Force
-                New-Variable -Name nLogWriteLog  -Value $False -Scope Script -Force
-            } Else {
-                New-Variable -Name nLogWriteHost -Value $False -Scope Script -Force
-                New-Variable -Name nLogWriteLog  -Value $True  -Scope Script -Force
-            }
-            If ([String]::IsNullOrEmpty([io.path]::GetFileNameWithoutExtension($script:MyInvocation.MyCommand.path))) {
-                New-Variable -Name nLogFile -Scope Script -Force -Value "$env:ALLUSERSPROFILE\Scripts\Logs\ISETestScript.log"
-            } Else {
-                New-Variable -Name nLogFile -Scope Script -Force -Value "$env:ALLUSERSPROFILE\Script\Logs\$([io.path]::GetFileNameWithoutExtension($script:MyInvocation.MyCommand.path))`.log"
-            }
-        }
-
-        #Initalize of the variables.
-        IF ($PSBoundParameters.ContainsKey('SetLogLevel')) {
-            Set-Variable -Name nLogLevel -Value $SetLogLevel -Force -Scope Script
-        }
-        IF ($PSBoundParameters.ContainsKey('SetWriteHost')) {
-            Set-Variable -Name nLogWriteHost -Value $SetWriteHost -Force -Scope Script
-        }
-        IF ($PSBoundParameters.ContainsKey('SetWriteLog')) {
-            Set-Variable -Name nLogWriteLog -Value $SetWriteLog -Force -Scope Script
-        }
-        IF ($PSBoundParameters.ContainsKey('SetLogFile')) {
-            Set-Variable -Name nLogFile -Value $SetLogFile -Force -Scope Script
-        }
-
-        #Determine log level
-        Switch ($Type) {
-            {$Type -eq 'Debug'   -AND $Script:nLogLevel -EQ 1} {$tLevel = "[DEBUG]`t`t"; $tForeGroundColor = "Cyan"   ; $tLog = $True; $tErrorString = [String]::Empty }
-            {$Type -eq 'Info'    -AND $Script:nLogLevel -LE 2} {$tLevel = "[INFO]`t`t" ; $tForeGroundColor = "White"  ; $tLog = $True; $tErrorString = [String]::Empty }
-            {$Type -eq 'Warning' -AND $Script:nLogLevel -LE 3} {$tLevel = "[WARNING]`t"; $tForeGroundColor = "DarkRed"; $tLog = $True; $tErrorString = [String]::Empty }
-            {$Type -eq 'Error'   -AND $Script:nLogLevel -LE 4} {$tLevel = "[ERROR]`t`t"; $tForeGroundColor = "Red"    ; $tLog = $True; $tErrorString = "[$($ErrorCode.ToString("0000"))] " }
-            {$Type -eq 'Fatal'   -AND $Script:nLogLevel -LE 5} {$tLevel = "[FATAL]`t`t"; $tForeGroundColor = "Red"    ; $tLog = $True; $tErrorString = "[$($ErrorCode.ToString("0000"))] " }
-        }
-
-        #Determine what we should be logging/writing. 
-        IF ($WriteHost) { $tWriteHost = $True } Else { $tWriteHost = $Script:nLogWriteHost } 
-        IF ($WriteLog)  { $tWriteLog  = $True } Else { $tWriteLog  = $Script:nLogWriteLog  }
-
-        $tTimeStampString = $tTimeStamp.ToString("yyyy-mm-dd hh:mm:ss")
-        
-        #Ensure we have the timestamp of last entry for debug time differences
-        IF (-Not (Test-Path variable:Script:nLogLastTimeStamp)) {
-            New-Variable -Name nLogLastTimeStamp -Value $tTimeStamp -Scope Script -Force
-        }
-
-        #Calculate the time difference 
-        $tDifference = " ($(((New-TimeSpan -Start $Script:nLogLastTimeStamp -End $tTimeStamp).Seconds).ToString(`"0000`"))`s)"
-
-        if ($tWriteLog -and $tLog) {
-            If (-NOT [System.IO.File]::Exists($Script:nLogFile)) {
-                New-Item -Path (Split-path $Script:nLogFile -Parent) -Name (Split-path $Script:nLogFile -Leaf) -Force -ErrorAction Stop
-            }
-            $tLogWriter = [System.IO.StreamWriter]::New($Script:nLogFile,"Append")
-        }
+    #Allows us to turn on verbose on all powershell commands when adding -verbose
+    IF ($PSBoundParameters.ContainsKey('Verbose')) {
+        Set-Variable -Name Verbose -Value $True
+    } Else {
+        Set-Variable -Name Verbose -Value $False
     }
-    Process {
-        IF ($tLog) {
-            IF ($tWriteHost) { 
-                Write-Host "$tTimeStampString$tDifference`t$tErrorString$Message" -ForegroundColor $tForeGroundColor
-            }
-        
-            IF ($tWriteLog)  {
-                $tLogWriter.WriteLine("$tTimeStampString$tDifference`t$tErrorCode$Message")
-            }
-            #Ensure we have the timestamp of the last log execution.
-            Set-Variable -Name nLogLastTimeStamp -Scope Script -Value $tTimeStamp -Force
+
+    New-Variable -Name StartTime    -Value ([DateTime]::Now)                                                                       -Force -Verbose:$Verbose -Description "Used to calculate timestamp differences between log calls."
+    New-Variable -Name StartTimeUTC -Value ([DateTime]::UtcNow.ToString("s",[System.Globalization.CultureInfo]::InvariantCulture)) -Force -Verbose:$Verbose -Description "Used to for logfile timestamp."
+
+    #Ensure all the required script-level variables are set.
+    IF ((-Not (Test-Path variable:Script:nLogInitialize)) -OR $Initialize) {
+        New-Variable -Name nLogLevel         -Verbose:$Verbose -Scope Script -Force -Value ([Int]3)
+        New-Variable -Name nLogInitialize    -Verbose:$Verbose -Scope Script -Force -Value $True
+        New-Variable -Name nLogWriteHost     -Verbose:$Verbose -Scope Script -Force -Value $False
+        New-Variable -Name nLogWriteLog      -Verbose:$Verbose -Scope Script -Force -Value $True
+        New-Variable -Name nLogDir           -Verbose:$Verbose -Scope Script -Force -Value $Env:TEMP
+        New-Variable -Name nLogLastTimeStamp -Verbose:$Verbose -Scope Script -Force -Value $StartTime
+        New-Variable -Name nLogFileValid     -Verbose:$Verbose -Scope Script -Force -Value $False
+        If ([String]::IsNullOrEmpty([io.path]::GetFileNameWithoutExtension($script:MyInvocation.MyCommand.path))) {
+            New-Variable -Name nLogFile -Scope Script -Force -Verbose:$Verbose -Value "Untitled.log"
+        } Else {
+            New-Variable -Name nLogFile -Scope Script -Force -Verbose:$Verbose -Value "$([io.path]::GetFileNameWithoutExtension($script:MyInvocation.MyCommand.path))`.log"
         }
+        New-Variable -Name nLogFullName      -Verbose:$Verbose -Scope Script -Force -Value (Join-Path -Path $Script:nLogDir -ChildPath $Script:nLogFile)
+        New-Variable -Name nLogLevels        -Verbose:$Verbose -Scope Script -Force -Value $([HashTable]@{
+            Debug   = @{ Text = "[DEBUG]  "; LogLevel = [Int]'1'; tForeGroundColor = "Cyan";   }
+            Info    = @{ Text = "[INFO]   "; LogLevel = [Int]'2'; tForeGroundColor = "White";  }
+            Warning = @{ Text = "[WARNING]"; LogLevel = [Int]'3'; tForeGroundColor = "DarkRed";}
+            Error   = @{ Text = "[ERROR]  "; LogLevel = [Int]'4'; tForeGroundColor = "Red";    }
+            Fatal   = @{ Text = "[FATAL]  "; LogLevel = [Int]'5'; tForeGroundColor = "Red";    }
+        })
     }
-    End {
-        if ($tWriteLog -and $tLog) {
-            $tLogWriter.Flush()
-            $tLogWriter.Close()
+
+    #Initalize of the variables.
+    IF ($PSBoundParameters.ContainsKey('SetLogLevel')) {
+        Set-Variable -Name nLogLevel     -Verbose:$Verbose -Scope Script -Force -Value $SetLogLevel
+    }
+    IF ($PSBoundParameters.ContainsKey('SetWriteHost')) {
+        Set-Variable -Name nLogWriteHost -Verbose:$Verbose -Scope Script -Force -Value $SetWriteHost
+    }
+    IF ($PSBoundParameters.ContainsKey('SetWriteLog')) {
+        Set-Variable -Name nLogWriteLog  -Verbose:$Verbose -Scope Script -Force -Value $SetWriteLog
+    }
+    IF ($PSBoundParameters.ContainsKey('SetLogDir')) {
+        Set-Variable -Name nLogDir       -Verbose:$Verbose -Scope Script -Force -Value $SetLogDir
+        Set-Variable -Name nLogFileValid -Verbose:$Verbose -Scope Script -Force -Value $False
+    }
+    IF ($PSBoundParameters.ContainsKey('SetLogFile')) {
+        Set-Variable -Name nLogFile      -Verbose:$Verbose -Scope Script -Force -Value "$($SetLogFile -replace "[$([string]::join('',([System.IO.Path]::GetInvalidFileNameChars())) -replace '\\','\\')]",'_').`log"
+        Set-Variable -Name nLogFileValid -Verbose:$Verbose -Scope Script -Force -Value $False
+    }
+
+    IF ($PSBoundParameters.ContainsKey('WriteHost')) { $tWriteHost = $True } Else { $tWriteHost = $Script:nLogWriteHost }
+    IF ($PSBoundParameters.ContainsKey('WriteLog'))  { $tWriteLog  = $True } Else { $tWriteLog  = $Script:nLogWriteLog  }
+
+    #Determine if script log level greater than or equal to current log event level and we actually are configured to write something.
+    IF ($Script:nLogLevels[$Type]["LogLevel"] -ge $Script:nLogLevel -AND ($tWriteHost -EQ $True -OR $tWriteLog -EQ $True)) {
+
+        #Code Block if writing out to log file.
+        if ($tWriteLog) {
+            IF ($Script:nLogFileValid -eq $False) {
+                Set-Variable -Name nLogFullName      -Verbose:$Verbose -Scope Script -Force -Value (Join-Path -Path $Script:nLogDir -ChildPath $Script:nLogFile)
+                If ([System.IO.File]::Exists($Script:nLogFullName)) {
+                    Set-Variable -Name nLogFileValid -Verbose:$Verbose -Scope Script -Force -Value $True
+                } Else {
+                    New-Item -Path $Script:nLogFullName -Force -Verbose:$Verbose
+                    Set-Variable -Name nLogFileValid -Verbose:$Verbose -Scope Script -Force -Value $True
+                }
+            }
+            $StreamWriter = [System.IO.StreamWriter]::New($Script:nLogFullName,$True,([Text.Encoding]::UTF8))
+            $StreamWriter.WriteLine("$StartTimeUTC||$Env:COMPUTERNAME||$Type||$($ErrorCode.ToString(`"0000`"))||$($MyInvocation.ScriptLineNumber)||$Message")
+            $StreamWriter.Close()
         }
 
-        #Cleanup Used Variables to make ISE development more consistent. 
-        Get-Variable -Name * -Scope Local |Where-Object { (@("WriteHost","WriteLog","Type","tTimeStampString","tTimeStamp","tLog","TerminatingError","tDifference","SetLogLevel","SetLogFile","ErrorCode","Message","Initialize","SetWriteHost","SetWriteLog","tWriteLog","tWriteHost","tLogWriter") -contains $_.Name) } |Remove-Variable
-        
-        #Allow us to exit the script from the logging function.
-        If ($Type -eq 'Fatal') {
-            Exit
+        #Code Block if writing out to log host.
+        IF ($tWriteHost) {
+            Write-Host -ForegroundColor $Script:nLogLevels[$Type]["tForeGroundColor"] -Verbose:$Verbose "$StartTime ($(((New-TimeSpan -Start $Script:nLogLastTimeStamp -End $StartTime -Verbose:$Verbose).Seconds).ToString('0000'))s) $($Script:nLogLevels[$Type]['Text']) [$($ErrorCode.ToString('0000'))] [Line: $($MyInvocation.ScriptLineNumber.ToString('0000'))] $Message"
         }
+                
+        #Ensure we have the timestamp of the last log execution.
+        Set-Variable -Name nLogLastTimeStamp -Scope Script -Value $StartTime -Force -Verbose:$Verbose
+    }
+    
+    #Remove Function Level Variables
+    Remove-Variable -Name @("Message","SetLogLevel","SetLogFile","Close","SetWriteLog","SetWriteHost","LineNumber","ErrorCode","tWriteHost","WriteHost","tWriteLog","WriteLog","StartTime") -ErrorAction SilentlyContinue
+
+    IF ($PSBoundParameters.ContainsKey('Close') -or $Type -eq 'Fatal') {
+        Remove-Variable -Name @("nLogLastTimeStamp","nLogFileValid","nLogFile","nLogDir","nLogWriteLog","nLogWriteHost","nLogInitialize","nLogLastTimeStamp","nLogLevels","nLogFullName","nLogLevel") -Scope Script -ErrorAction SilentlyContinue
+    }
+
+    #Allow us to exit the script from the logging function.
+    If ($Type -eq 'Fatal') {
+        Exit
     }
 }
+
+Function Start-PerformanceTest {
+    <#
+        .SYNOPSIS
+            Test the execution time of script blocks.
+        .DESCRIPTION
+            Perform an accurate measurement of a block of code over a number of itterations allowing informed decisions to be made about code efficency. 
+        .PARAMETER ScriptBlock
+            [ScriptBlock] Code to run and measure. Input code as either a ScriptBlock object or wrap it in {} and the script will attempt to convert it automatically.
+        .PARAMETER Measurement
+            [String] Ime interval in which to display measurements. (Options: Milliseconds, Seconds, Minutes, Hours, Days)
+        .PARAMETER Itterations
+            [Int] Numbers of times to run the code.
+        
+        .INPUTS
+            None
+        .OUTPUTS
+            None
+        .NOTES
+        VERSION     DATE			NAME						DESCRIPTION
+	    ___________________________________________________________________________________________________________
+	    1.0         20 August 2020	Warilia, Nicholas R.		Initial version
+        Credits:
+            (1) Script Template: https://gist.github.com/9to5IT/9620683
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ScriptBlock]$ScriptBlock,
+        [ValidateSet("Milliseconds",“Seconds”,”Minutes”,”Hours”,"Days")]
+        $Measurement = "Seconds",
+        [int]$Iterations = 100
+    )
+
+    $Results = [System.Collections.ArrayList]::new()
+
+    For ($I=0;$I -le $Iterations;$I++) {
+        [Void]$Results.Add(
+            ((Measure-Command -Expression ([scriptblock]::Create($ScriptBlock)) |Select-Object TotalDays,TotalMinutes,TotalSeconds,TotalMilliseconds))
+        )
+    }
+
+    #Determine correct timestamp label
+    Switch ($Measurement) {
+        'Milliseconds' {$LengthType = "ms"}
+        default        {$LengthType = $Measurement.SubString(0,1).tolower()}
+    }
+
+    $Results |Group-Object Total$Measurement |Measure-Object -Property Name -Average -Maximum -Minimum | Select-Object `
+            @{Name="Maximum";Expression={"$([Math]::Round($_.Maximum,3))$LengthType"}},
+            @{Name="Minimum";Expression={"$([Math]::Round($_.Minimum,3))$LengthType"}},
+            @{Name="Average";Expression={"$([Math]::Round($_.Average,3))$LengthType"}}
+}
+
+Write-nLog -Initialize -Type Info -Message "Testing" -SetLogLevel 1 -WriteHost -SetWriteHost $False -SetWriteLog $True
+Start-PerformanceTest -ScriptBlock { Write-nLog -Type Debug -Message "Testing Time Spacer" } -Measurement Milliseconds -Iterations 10000
+Write-nLog -Type Debug -Message "Testing234" -WriteHost
+Write-nLog -Type Info -Message "Testing234" -WriteHost
+Write-nLog -Type Warning -Message "Testing234" -WriteHost
+Write-nLog -Type Error -Message "Testing234" -WriteHost
+Write-nLog -Close -Message Test -Type Debug
